571d664ed4f9478936eae4c6998f186afa2b6e15
MM13 <Fastsoccer13@gmail.com>
2019-11-01 18:47:13 -0400

    completed .on click fucntion and made a .on child added function to get data from the database and put in the html. Made the varibles for calculating the time but I feel like I still need to work on it some more the get in perfect.

--- Javascript/TTime.js
+++ Javascript/TTime.js
@@ -6,37 +6,71 @@ var firebaseConfig = {
     storageBucket: "test-project-89273.appspot.com",
     messagingSenderId: "703956088604",
     appId: "1:703956088604:web:e520d827050971b8d22cd9"
-    };    
+};
 console.log("hello world");
-    firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
-    let database = firebase.database();
+let database = firebase.database();
+
+//variable holders for user input
+let name = "";
+let destination = "";
+let frequency = 0;
+let firstTrainTime = "";
+
+//eventHandler
+$("#train-submit").on("click", function (event) {
+    event.preventDefault();
+
+    name = $("#nameTrain").val().trim();
+    destination = $("#whereTo").val().trim();
+    frequency = $("#timeFreak").val().trim();
+    firstTrainTime = $("#military").val().trim();
+    //takes user input and sends to the database lable as the varibles blow
+    let newTrainAdd = {
+        name: name,
+        destination: destination,
+        frequency: frequency,
+        firstTrainTime: firstTrainTime,
+    };
+    database.ref().push(newTrainAdd);
+    console.log(newTrainAdd.name)
+    //will clear text boxes
+    $("#nameTrain").val("");
+    $("#whereTo").val("");
+    $("#timeFreak").val("");
+    $("#military").val("");
+
+    //will alert that a new train was added 
+    alert("New Train Added");
+});
+//creates an event when train is added to the database, it will copy the input information and put it in the html specified area.
+database.ref().on("child_added", function(theChildInfo) {
+    console.log(theChildInfo.val());
+
+    let name = theChildInfo.val().name;
+    let destination = theChildInfo.val().destination;
+    let frequency = theChildInfo.val().frequency;
+    let firstTrainTime = theChildInfo.val().firstTrainTime;
+
+    let firstTimeInput = moment(firstTrainTime, "HH:mm").subtract(1, "years");
     
-    //variable holders for user input
-    let name = "";
-    let destination = "";
-    let frequency = 0;
-    let firstTrainTime = "";
+    let currentTime = moment();
+    let diffTime = moment().diff(moment(firstTimeInput), "minutes");
+    let remainder = diffTime % frequency;
+    let minutesAway = frequency - remainder;
+    let nextArrival = moment().add(minutesAway, "minutes");
 
-   //eventHandler
-    $("#train-submit").on("click", function(event) {
-        event.preventDefault();
+    let addRow = $("<tr>").append(
+        $("<td>").text(name),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(nextArrival),
+        $("<td>").text(minutesAway),
+    );
 
-        name = $("#nameTrain").val().trim();
-        destination = $("#whereTo").val().trim();
-        frequency = $("#timeFreak").val().trim();
-        firstTrainTime = $("#military").val().trim();
-
-        database.ref().push({
-            name: nameT,
-            destination: destinationT,
-            frequency: frequencyT,
-            firstTrainTime: firstTrainTimeT,
-            dataAdded: firebase.database.ServerValue.TIMESTAMP
+    addRow.appendTo("#trainMade");
+});
 
 
-        });
 
-    });
-
-    console.log("hello world");
